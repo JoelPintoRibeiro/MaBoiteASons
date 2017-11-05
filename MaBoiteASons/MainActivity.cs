b9049d2a8d2ea94d9cfeacc26c24effe0dfb0839
Joel Pinto Ribeiro <ribeiro.joel@gmail.com>
2017-11-05 11:11:55 +0100

    Ajout gridview

--- MaBoiteASons/MainActivity.cs
+++ MaBoiteASons/MainActivity.cs
@@ -7,20 +7,42 @@ namespace MaBoiteASons
     [Activity(Label = "@string/toolbar_text", MainLauncher = true)]
     public class MainActivity : Activity
     {
+
+        private GridView _songsGridView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
 
-            var audioManager = new AudioManager();
+            FindViews();
 
+            _songsGridView.Adapter = new ImageAdapter(this);
+
+            MakeHandlers();
+
+
+            var audioManager = new AudioManager();
+            audioManager.AddAudio(new Models.AudioFile { FileName = "test", Name = "name" });
             if (audioManager.GetAllAudios().Count == 0)
             {
-
+                SetContentView(Resource.Layout.Main);
+            }
+            else
+            {
+                SetContentView(Resource.Layout.MainSongs);
             }
 
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+        }
+        private void MakeHandlers()
+        {
+
+            _songsGridView.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs args) {
+                Toast.MakeText(this, args.Position.ToString(), ToastLength.Short).Show();
+            };
+        }
+        private void FindViews()
+        {
+            _songsGridView = FindViewById<GridView>(Resource.Id.gridviewSongs);
         }
     }
 }
--- MaBoiteASons/Resources/Resource.Designer.cs
+++ MaBoiteASons/Resources/Resource.Designer.cs
@@ -44,8 +44,8 @@ namespace MaBoiteASons
 		public partial class Color
 		{
 			
-			// aapt resource value: 0x7f040000
-			public const int theme_custom_background = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int theme_custom_background = 2131034112;
 			
 			static Color()
 			{
@@ -57,20 +57,60 @@ namespace MaBoiteASons
 			}
 		}
 		
+		public partial class Drawable
+		{
+			
+			// aapt resource value: 0x7f020000
+			public const int sample_0 = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int sample_1 = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int sample_2 = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int sample_3 = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int sample_4 = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int sample_5 = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int sample_6 = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int sample_7 = 2130837511;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060003
-			public const int button1 = 2131099651;
+			// aapt resource value: 0x7f070003
+			public const int button1 = 2131165187;
 			
-			// aapt resource value: 0x7f060000
-			public const int linearLayout1 = 2131099648;
+			// aapt resource value: 0x7f070004
+			public const int gridviewSongs = 2131165188;
 			
-			// aapt resource value: 0x7f060002
-			public const int linearLayout2 = 2131099650;
+			// aapt resource value: 0x7f070000
+			public const int linearLayout1 = 2131165184;
 			
-			// aapt resource value: 0x7f060001
-			public const int title = 2131099649;
+			// aapt resource value: 0x7f070002
+			public const int linearLayout2 = 2131165186;
+			
+			// aapt resource value: 0x7f070001
+			public const int title = 2131165185;
 			
 			static Id()
 			{
@@ -85,8 +125,11 @@ namespace MaBoiteASons
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			// aapt resource value: 0x7f030000
+			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int MainSongs = 2130903041;
 			
 			static Layout()
 			{
@@ -101,14 +144,14 @@ namespace MaBoiteASons
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030002
-			public const int add_audio = 2130903042;
+			// aapt resource value: 0x7f040002
+			public const int add_audio = 2130968578;
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int app_name = 2130968576;
 			
-			// aapt resource value: 0x7f030001
-			public const int toolbar_text = 2130903041;
+			// aapt resource value: 0x7f040001
+			public const int toolbar_text = 2130968577;
 			
 			static String()
 			{
@@ -123,8 +166,8 @@ namespace MaBoiteASons
 		public partial class Style
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int CustomTheme = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int CustomTheme = 2131099648;
 			
 			static Style()
 			{
